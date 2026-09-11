27914af69beece6eaf94854d758490997be048da
UjjwalAlpha <ujjwalllllll07@gmail.com>
2026-09-11 23:28:11 +0530

    Time: 0 ms (100%), Space: 47.7 MB (77.44%) - LeetHub

---
+++ 0050-powx-n/0050-powx-n.java
@@ -0,0 +1,17 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n<0){
+            n = n*-1;
+            x = 1/x;
+        }
+        double ans = 1;
+        while(n!=0){
+            if(n%2!=0){
+                ans = ans*x;
+            }
+            x = x*x;
+            n = n/2;
+        }
+        return ans;
+    }
+}
